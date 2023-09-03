4468bca81a01c4d5139fd071fbadc7cd71a96b65
shawon21712161 <sshawon19@gmail.com>
2023-09-04 03:14:56 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,45 @@
+// alternative fetch
+
+// function loadData() {
+    
+//     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+//     .then(res => res.json())
+//     .then(data => console.log(data.data))  
+// }
+
+// loadData()
+
+
+// async fetch
+
+const loadData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json();
+    const phones = data.data; 
+    displayPhones(phones)
+}
+
+const displayPhones = phones =>{
+    const PhoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList=`card w-96 bg-gray-200 shadow-xl`;
+        phoneCard.innerHTML=`<figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>   
+        `
+        PhoneContainer.appendChild(phoneCard);
+    });
+}
+
+
+
+
+loadData()
+
